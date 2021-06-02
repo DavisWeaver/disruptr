452840612de92f2c706c1befe5b775c1c1976f8b
Install <Install@LRI-108942.lerner.ad.cchs.net>
2021-06-02 14:23:14 -0400

    sj_final added to system files

--- manual_tests/sj_mir_test.R
+++ manual_tests/sj_mir_test.R
@@ -14,3 +14,7 @@ jude_df <- pivot_wider(jude_df, id_cols = Geneid, names_from = sample_name,
 
 jude_dfnp <- compute_np(cache = cache, experiment_name = "sj", ppi = "biogrid",
                         exp_mat= jude_df, mir_paper = FALSE, ncores = 4)
+
+jdf_dnp <- compute_dnp(cache = cache, df = jude_dfnp, experiment_name = "sj",
+                       ppi = "biogrid", ncores = 5)
+
