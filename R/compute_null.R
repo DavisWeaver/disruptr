60828e78c01504eff116e5f0d7357844a827d588
Davis Weaver <dtw43@case.edu>
2021-06-08 14:28:55 -0400

    null_updates

--- R/compute_null.R
+++ R/compute_null.R
@@ -17,20 +17,19 @@
 #'
 #' @param df output of [compute_dnp()]
 #' @param n number of permutations
-#' @param all_samples default behavior is to compute the null distribution for
-#'    just one sample in the data due to computational expense. Set to true to compute for
-#'    all samples
 #' @param v character vector of protein names that we will compute the null
-#'     distribution for, defaults to all
+#'     distribution for, defaults to the top 50, as ranked by change in network potential from
+#'     previous steps in the pipeline. Change n_genes parameter to compute for more or less
+#' @param n_genes integer describing number of genes per sample that we will compute the null distribution for
 #' @inheritParams compute_dnp
 #'
-#' @importFrom foreach %dopar% %:%
+#' @importFrom foreach %dopar% %:% %do%
 #' @importFrom magrittr %>%
 #'
 #' @export
 
-compute_null <- function(cache = NULL, df, ppi = "biogrid", n, all_samples = FALSE,
-                         experiment_name, ncores = 1) {
+compute_null <- function(cache = NULL, df, ppi = "biogrid", n, all_genes = FALSE,
+                         n_genes = 50, experiment_name, ncores = 1) {
   if(is.null(cache)) {
     stop("please provide a cache for saving and loading data/output")
   }
@@ -39,18 +38,14 @@ compute_null <- function(cache = NULL, df, ppi = "biogrid", n, all_samples = FAL
     load(paste0(cache, experiment_name, "dnpNull.Rda"))
     return(df_np)
   }
+
   #load ppi
   g <- load_ppi(cache = cache, min_score = min_score, ppi = ppi)
 
-  #grab the first sample in the data if all_samples== FALSE
-  if(all_samples == FALSE) {
-    our_samp <- unique(df$sample_name)[1]
-    df <- dplyr::filter(df, .data$sample_name == our_samp)
-  }
-
-  #grab list of genes to iterate over and a list of samples to iterate over
-  genes <- unique(df$gene_name)
   samples <- unique(df$sample_name)
+
+  #grab top n genes per sample to iterate over
+  v_list = get_topn(df =df, n_genes = n_genes)
 
   #get rid of dnp
   df <- df %>% dplyr::select(-dnp)
@@ -61,16 +56,21 @@ compute_null <- function(cache = NULL, df, ppi = "biogrid", n, all_samples = FAL
   cl <- parallel::makeCluster(ncores)
   doParallel::registerDoParallel(cl)
   out <-
-    foreach::foreach(j = 1:length(df_list)) %:%
-    foreach::foreach(i = 1:n, .combine = "rbind") %dopar%
-    {
-      df_j <- df_list[[j]]
-      #scramble the graph
-      g_i <- get_random_graph(g)
+    foreach::foreach(j = iterators::iter(df_list), .packages = "disruptr") %dopar% {
+      cell_line_df <-
+        foreach::foreach(i = 1:n, .combine = "rbind", .packages = "disruptr") %do%
+        {
+          #scramble the graph
+          g_i <- get_random_graph(g)
 
-      #calc change in network potential
-      dnp_ij <- calc_dnp_i(df_j, g_i)
+          #calc change in network potential
+          dnp_ij <- calc_dnp_i(j, g_i)
+          dnp_ij$i = i
+          return(dnp_ij)
+        }
+
     }
+
   #close out parallel execution
   parallel::stopCluster(cl)
 
@@ -89,4 +89,15 @@ compute_null <- function(cache = NULL, df, ppi = "biogrid", n, all_samples = FAL
 get_random_graph <- function(g) {
   g2 <- igraph::rewire(g, igraph::keeping_degseq(niter = igraph::vcount(g)*10))
   return(g2)
+}
+
+#' Helper function for compute_null - returns the top n genes by dnp for each sample
+#'
+#' @importFrom magrittr %>%
+
+get_topn <- function(df, n_genes) {
+  df %>% dplyr::group_by(.data$sample_name) %>%
+    dplyr::slice_max(order_by = .data$dnp, n = n_genes) %>%
+    dplyr::select(.data$gene_name, .data$sample_name) %>%
+    dplyr::group_split()
 }
