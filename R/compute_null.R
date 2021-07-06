3647c7ff203031818de14aec2a42e4ee6580a79f
Dave Weaver <dweav94@gmail.com>
2021-07-06 10:11:48 -0400

    compute_null memory fix

--- R/compute_null.R
+++ R/compute_null.R
@@ -62,7 +62,7 @@ compute_null <- function(cache = NULL, df, ppi = "biogrid", n,
 
       mem_df <- list()
       agg_df <- list()
-      j <- 1
+      z <- 1
       for(i in 1:n) {
 
         #get the info on the top genes for a given sample
@@ -72,166 +72,107 @@ compute_null <- function(cache = NULL, df, ppi = "biogrid", n,
         g_i <- get_random_graph(g)
 
         #calc change in network potential
-        #dnp_ij <- calc_dnp_i(j, g_i, v_rm = v_j, keep_all = FALSE)
+        dnp_ij <- calc_dnp_i(j, g_i, v_rm = v_j, keep_all = FALSE)
 
         #for testing only
-        dnp_ij <- dplyr::filter(j, .data$gene_name %in% v_j)
-        dnp_ij$dnp <- rnorm(nrow(dnp_ij), mean = 50, sd = 20)
+        # dnp_ij <- dplyr::filter(j, .data$gene_name %in% v_j)
+        # dnp_ij$dnp <- rnorm(nrow(dnp_ij), mean = 50, sd = 20)
 
-        mem_df <- c(mem_df, dnp_ij)
+        mem_df[[i]] <- dnp_ij
 
         if(i %% 5 == 0) {
-          agg_df[[j]] <- combine_null(mem_df)
+          agg_df[[z]] <- combine_null(mem_df)
+          agg_df[[z]]$z <- z
           mem_df <- list() #clear "mememory
+          z <- z + 1#progress ticker
         }
-
-        return(list(i, dnp_ij))
       }
-      # null_j_df <- cell_line_df %>%
-      #   dplyr::group_by(.data$gene_name, .data$sample_name) %>%
-      #   dplyr::summarise(mean_dnp = mean(.data$np),
-      #                    sd_dnp = sd(.data$dnp),
-      #                    n = dplyr::n())
-      #return(null_j_df)
-
-
-
-      #close out parallel execution
-      parallel::stopCluster(cl)
-
-      #save
-      save(null_df, file = paste0(cache, experiment_name, "dnpNull.Rda"))
-
-      #return
-      return(null_df)
-    }
-
-  #' Helper function for compute_null - returns a graph with randomly permuted edges.
-  #'
-  #' currently just a wrapper for igraph::rewire but may add more functionality in the future
-  #'
-  #' @param g graph to be permuted
-  #'
-  #' @seealso [igraph::rewire()]
-  #' @export
-
-  get_random_graph <- function(g) {
-    g2 <- igraph::rewire(g, igraph::keeping_degseq(niter = igraph::vcount(g)*100))
-    return(g2)
-  }
-
-  #' Helper function for compute_null - returns the top n genes by dnp for each sample
-  #' @inheritParams compute_null
-  #' @importFrom magrittr %>%
-
-  get_topn <- function(df, n_genes) {
-    df %>% dplyr::group_by(.data$sample_name) %>%
-      dplyr::slice_max(order_by = .data$dnp, n = n_genes) %>%
-      dplyr::select(.data$gene_name, .data$sample_name) %>%
-      tidyr::pivot_wider(names_from = .data$sample_name,
-                         values_from = .data$gene_name,
-                         values_fn = list)
-  }
-  #' .combine function for compute_null foreach looping structure
-  #'
-  #' @param x aggregated data structure
-  #' @param y task returned by inner foreach loop of compute_null
-  #'
-  #' @param ... arbitrary number of tasks returned by the inner foreach loop of
-  #'            compute_null
-  #' @importFrom magrittr %>%
-  #' @export
-
-  combine_null <- function(x, y, ...) {
-
-    #bind together all the arguments that aren't the aggregate
-    args <- lapply(list(y, ...), function(z) z[[2]])
-
-    #don't want to try and compute sd with one sample... thats where the trouble is ocming I t
-    if(y[[1]] == 2) {
-      x[[1]] <- dplyr::bind_rows(x[[2]], args) %>%
-        dplyr::group_by(.data$gene_name, .data$sample_name) %>%
-        dplyr::summarise(mean_dnp = mean(.data$dnp),
-                         sd_dnp = sd(.data$dnp, na.rm = TRUE),
-                         n = dplyr::n())
-      x[[2]] <- list() #need to clear out x[[2]] any time we add to x1
-    } else {
-      x[[2]] <- c(x[[2]], args)
-    }
-
-    if(length(x[[2]]) >= 10) { #condition where x is pre-combined version
-      x_agg = x[[1]]
-      #bind everything together
-      df <- dplyr::bind_rows(x[[2]])
-      x[[2]] <- list() #clear out the "memory" any time we re-compute x[[1]]
-      df <- df %>%
-        dplyr::group_by(.data$gene_name, .data$sample_name) %>%
-        dplyr::summarise(mean_dnp = mean(.data$dnp),
-                         sd_dnp = sd(.data$dnp, na.rm = TRUE),
-                         n = dplyr::n())
-
-      #re-order df to be in the same order as x
-      new_index = match(df$gene_name, x_agg$gene_name)
-      df <- df[order(new_index),]
-
-      #compute weighted averages of everything
-      mean_dnp <- vector(mode = "numeric", length = nrow(df))
-      sd_dnp <- vector(mode = "numeric", length = nrow(df))
-      n_vec <- vector(mode= "integer", length = nrow(df))
-      for(i in 1:length(mean_dnp)) {
-        mean_dnp[i] <-
-          (x_agg$mean_dnp[i]*x_agg$n[i] + df$mean_dnp[i]*df$n[i]) /
-          (x_agg$n[i] + df$n[i])
-        sd_dnp[i] <-
-          (x_agg$sd_dnp[i]*x_agg$n[i] + df$sd_dnp[i]*df$n[i]) /
-          (x_agg$n[i] + df$n[i])
-        n_vec[i] <- x_agg$n[i] + df$n[i]
-      }
-
-      x[[1]] <- data.frame(gene_name = df$gene_name, sample_name = df$sample_name,
-                           mean_dnp = mean_dnp, sd_dnp = sd_dnp, n = n_vec)
+      return(final_combine(agg_df)) #pass back to the main foreach loop
 
     }
-    return(x)
-  }
+  # null_j_df <- cell_line_df %>%
+  #   dplyr::group_by(.data$gene_name, .data$sample_name) %>%
+  #   dplyr::summarise(mean_dnp = mean(.data$np),
+  #                    sd_dnp = sd(.data$dnp),
+  #                    n = dplyr::n())
+  #return(null_j_df)
 
-  #' final .combine function to run in compute_null foreach looping structure
-  #'
-  #' @param x aggregated info
-  #' @param ... arbitrary number of tasks returned by the inner foreach loop of
-  #'            compute_null
-  #' @importFrom magrittr %>%
-  #' @export
-  #'
-  final_combine <- function(x) {
-    x_agg = x[[1]]
-    #bind everything together
-    df <- dplyr::bind_rows(x[[2]])
-    df <- df %>%
-      dplyr::group_by(.data$gene_name, .data$sample_name) %>%
-      dplyr::summarise(mean_dnp = mean(.data$dnp),
-                       sd_dnp = sd(.data$dnp, na.rm = TRUE),
-                       n = dplyr::n())
 
-    #re-order df to be in the same order as x
-    new_index = match(df$gene_name, x_agg$gene_name)
-    df <- df[order(new_index),]
 
-    #compute weighted averages of everything
-    mean_dnp <- vector(mode = "numeric", length = nrow(df))
-    sd_dnp <- vector(mode = "numeric", length = nrow(df))
-    n_vec <- vector(mode= "integer", length = nrow(df))
-    for(i in 1:length(mean_dnp)) {
-      mean_dnp[i] <-
-        (x_agg$mean_dnp[i]*x_agg$n[i] + df$mean_dnp[i]*df$n[i]) /
-        (x_agg$n[i] + df$n[i])
-      sd_dnp[i] <-
-        (x_agg$sd_dnp[i]*x_agg$n[i] + df$sd_dnp[i]*df$n[i]) /
-        (x_agg$n[i] + df$n[i])
-      n_vec[i] <- x_agg$n[i] + df$n[i]
-    }
+  #close out parallel execution
+  parallel::stopCluster(cl)
 
-    x <- data.frame(gene_name = df$gene_name, sample_name = df$sample_name,
-                    mean_dnp = mean_dnp, sd_dnp = sd_dnp, n = n_vec)
-    return(x)
-  }
+  #save
+  save(null_df, file = paste0(cache, experiment_name, "dnpNull.Rda"))
+
+  #return
+  return(null_df)
+}
+
+#' Helper function for compute_null - returns a graph with randomly permuted edges.
+#'
+#' currently just a wrapper for igraph::rewire but may add more functionality in the future
+#'
+#' @param g graph to be permuted
+#'
+#' @seealso [igraph::rewire()]
+#' @export
+
+get_random_graph <- function(g) {
+  g2 <- igraph::rewire(g, igraph::keeping_degseq(niter = igraph::vcount(g)*100))
+  return(g2)
+}
+
+#' Helper function for compute_null - returns the top n genes by dnp for each sample
+#' @inheritParams compute_null
+#' @importFrom magrittr %>%
+
+get_topn <- function(df, n_genes) {
+  df %>% dplyr::group_by(.data$sample_name) %>%
+    dplyr::slice_max(order_by = .data$dnp, n = n_genes) %>%
+    dplyr::select(.data$gene_name, .data$sample_name) %>%
+    tidyr::pivot_wider(names_from = .data$sample_name,
+                       values_from = .data$gene_name,
+                       values_fn = list)
+}
+#' .combine function for compute_null foreach looping structure
+#'
+#' @param x aggregated data structure
+#' @param y task returned by inner foreach loop of compute_null
+#'
+#' @param ... arbitrary number of tasks returned by the inner foreach loop of
+#'            compute_null
+#' @importFrom magrittr %>%
+#' @export
+
+combine_null <- function(x) {
+
+  #don't want to try and compute sd with one sample... thats where the trouble is ocming I t
+  x <- dplyr::bind_rows(x) %>%
+    dplyr::group_by(.data$gene_name, .data$sample_name) %>%
+    dplyr::summarise(mean_dnp = mean(.data$dnp),
+                     sd_dnp = sd(.data$dnp, na.rm = TRUE),
+                     n = dplyr::n())
+
+  return(x)
+}
+
+#' final .combine function to run in compute_null foreach looping structure
+#'
+#' @param x aggregated info
+#' @param ... arbitrary number of tasks returned by the inner foreach loop of
+#'            compute_null
+#' @importFrom magrittr %>%
+#' @export
+#'
+#'
+final_combine <- function(x) {
+
+  x <- dplyr::bind_rows(x)
+  x <- x %>%
+    dplyr::group_by(.data$gene_name, .data$sample_name) %>%
+    dplyr::summarise(mean_dnp = mean(.data$mean_dnp),
+                     sd_dnp = mean(.data$sd_dnp, na.rm = TRUE),
+                     n = sum(.data$n))
+  return(x)
+}
